d595b8e951358d9bd1e782b2c454652046ee992d
Julfendi <babyoctopuses@gmail.com>
2023-04-09 22:07:33 +0700

    lesson-4-guessing-gamw

--- main.c
+++ main.c
@@ -1,82 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+void printPrompt();
+int generateNumber(int min, int max);
+int getGuesses(int guess);
+void checkGuess(int myGuess, int target);
 
 //learn about function
-void printMenu();
-int Square(int side);
-int Triangle(int bottom, int height);
-int Rectangle(int width, int breadth);
-
 int main(){
+    int num;
+    int myGuess;
     
-    int choice;
-    int num1, num2;
-    int area;
+    srand(time(NULL));
 
-    do{
-        printMenu();
+    int target = generateNumber(1,100);
+    int guess = 0;
 
-        printf("select a number: ");
-        scanf("%d", &choice);
-
-        switch(choice){
-            case 1:
-                printf("\nEnter the bottom: ");
-                scanf("%d", &num1);
-                printf("\nEnter the height: ");
-                scanf("%d", &num2);
-                
-                area = Triangle(num1, num2);
-                printf("The area of the triangle is %d", area);
-            break;
-            case 2:
-                printf("\nEnter the side: ");
-                scanf("%d", &num1);
-                
-                area = Square(num1);
-                printf("The area of the square is %d", area);
-                break;
-            case 3:
-                printf("\nEnter the width: ");
-                scanf("%d", &num1);
-                printf("\nEnter the breadth: ");
-                scanf("%d", &num2);
-
-                area = Rectangle(num1, num2);
-                printf("The area of the rectangle is %d", area);
-                break;
-            case 4:
-                break;
-            default:
-                printf("Please choose one of the options above");
-                break;
-        }
-    }while(choice != 4);
     
+    printPrompt();
 
-    printf("thank you for using this calculator");
+    while(1){
+        myGuess = getGuesses(guess);
+        checkGuess(myGuess, target);
+        guess++;
+        if (guess == target) {
+        break;
+    }
+    };
+
+    printf("Thank you for playing");
+
     return 0;
 }
 
-void printMenu(){
-    printf("\n\tMenu\n");
-    printf("1.Triangle\n");
-    printf("2.Square\n");
-    printf("3.Rectangle\n");
-    printf("4. EXIT\n");
+void printPrompt(){
+    printf("\nplease choose 1-100\n");
+    printf("can you guess the number?\n");
 }
 
-int Square(int side){
-    int num = side * side;
+int generateNumber(int min, int max){
+    int num = (rand() % (max-min+1))+min;
     return num;
 }
 
-int Triangle(int bottom, int height){
-    int num = bottom * height / 2;
-    return num;
+int getGuesses(int guess){
+    printf("\nGuess #%d: ", guess+1);
+    
+    int myGuess;
+    scanf("%d", &myGuess);
+    return myGuess;
 }
 
-int Rectangle(int width, int breadth){
-    int num = width * breadth;
-    return num;
+void checkGuess(int myGuess, int target){
+    if(myGuess<target){
+        printf("\ntoo low, enter higer number");
+    }else if(myGuess > target){
+        printf("\ntoo high, enter a lower number");
+    }else{
+        printf("Correct");
+    }
 }
